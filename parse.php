2a3786c37e06b6d7aba630e60379ac7257c111a5
cloudharmony <jason@cloudharmony.com>
2014-04-17 14:30:25 -0700

    first commit

---
+++ parse.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php -q
+<?php
+/**
+ * parses UnixBench output and produces relevant metrics
+ */
+$status = 1;
+if (isset($argv[1]) && file_exists($argv[1]) && ($buffer = file_get_contents($argv[1])) && 
+    preg_match_all('/Index Score\s+([0-9\.]+)\s/msU', $buffer, $m)) {
+  printf("score=%s\n", trim($m[1][0]));
+  // multicore
+  if (count($m[1]) > 1 && preg_match('/running\s+([0-9]+)\s+parallel copies/msU', $buffer, $m1)) {
+    printf("multicore_copies=%d\n", trim($m1[1]));
+    printf("multicore_score=%s\n", trim($m[1][1]));
+  }
+}
+exit($status);
+?>
